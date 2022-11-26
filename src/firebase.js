faea620dd7507a19e4521168729d89baccb82629
sejinniemma <jsj0471@naver.com>
2022-11-26 20:25:33 +0900

    set up project

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+firebaseConfig = {
+  apiKey: 'AIzaSyDt7pz60_uP9HzhZ6Gre3-9oo1xeWShG3A',
+  authDomain: 'shoppy-b869f.firebaseapp.com',
+  projectId: 'shoppy-b869f',
+  storageBucket: 'shoppy-b869f.appspot.com',
+  messagingSenderId: '470278969704',
+  appId: '1:470278969704:web:2f750152e5d76e547b5edd',
+};
+
+export default initializeApp(firebaseConfig);
