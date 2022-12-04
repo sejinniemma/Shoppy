843bc93f977096c02fec3e60a6f9d780f3aae9f5
sejinniemma <jsj0471@naver.com>
2022-12-04 19:39:31 +0900

    make button component and navbar UI

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,16 +4,13 @@ import { FiShoppingBag } from 'react-icons/fi';
 import { BsFillPencilFill } from 'react-icons/bs';
 import { login, logout, onUserStateChange } from '../api/firebase';
 import User from './User';
+import Button from './ui/Button';
 
 export default function Navbar() {
   const [user, setUser] = useState();
-  const [admin, setAdmin] = useState();
 
   useEffect(() => {
-    onUserStateChange((user) => {
-      user.isAdmin && setAdmin(user);
-      setUser(user);
-    });
+    onUserStateChange(setUser);
   }, []);
 
   return (
@@ -25,14 +22,22 @@ export default function Navbar() {
       <nav className='flex items-center gap-4 font-semibold'>
         <Link to='/products'>Products</Link>
         {user && <Link to='/carts'>Carts</Link>}
-        {user && (
+        {user && user.isAdmin && (
           <Link to='/products/new' className='text-2xl'>
             <BsFillPencilFill />
           </Link>
         )}
         {user && <User user={user} />}
-        {!user && <button onClick={login}>Login</button>}
-        {user && <button onClick={logout}>Logout</button>}
+        {!user && (
+          <Button text={'Login'} onClick={login}>
+            Login
+          </Button>
+        )}
+        {user && (
+          <Button text={'Logout'} onClick={logout}>
+            Logout
+          </Button>
+        )}
 
         {/* what is the difference?? */}
         {/* {user ? (
